bc7be178cce7f01434d40351936c7b6d96beb874
Ospino <ospinotech@gmail.com>
2022-04-04 16:51:10 -0500

    Commit inicial

---
+++ app/schema.py
@@ -0,0 +1,13 @@
+instructions=[
+    'DROP TABLE IF EXISTS email;',
+    """
+        CREATE TABLE email(
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            email TEXT NOT NULL,
+            subject TEXT NOT NULL,
+            content TEXT NOT NULL
+        );
+    
+    
+    """
+]
